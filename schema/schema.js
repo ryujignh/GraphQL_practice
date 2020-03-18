ae8777811f7ecbd8a5029bfa4671dbc7f64ed193
ryujignh <ryujignh@gmail.com>
2020-03-18 21:59:56 +0900

    Add axios

--- schema/schema.js
+++ schema/schema.js
@@ -1,16 +1,11 @@
 const graphql = require('graphql');
-const _ = require('lodash');
+const axios = require('axios');
 const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLInt,
     GraphQLSchema,
 } = graphql;
-
-const users = [
-    {id: '23', firstName: 'Bill', age: 20},
-    {id: '47', firstName: 'Samantha', age: 21}
-];
 
 const UserType = new GraphQLObjectType({
     name: 'User',
@@ -29,7 +24,8 @@ const RootQuery = new GraphQLObjectType({
             args: {id: {type: GraphQLString}},
             // Going to a database and get a data matching arguments
             resolve(parentValue, args) {
-                return _.find(users, {id: args.id});
+                return axios.get(`http://localhost:3000/users/${args.id}`)
+                    .then(resp => resp.data);
             }
         },
     }
