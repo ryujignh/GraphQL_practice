af45b7129801c33136bdb9d4f2ebdc29b1ef93b3
ryujignh <ryujignh@gmail.com>
2020-03-19 11:24:19 +0900

    Add mutation deleteUser

--- schema/schema.js
+++ schema/schema.js
@@ -86,7 +86,16 @@ const mutation = new GraphQLObjectType({
                 return axios.post('http://localhost:3000/users', {firstName, age})
                     .then(res => res.data);
             }
-
+        },
+        deleteUser: {
+            type: UserType,
+            args: {
+                id: {type: GraphQLNonNull(GraphQLString)}
+            },
+            resolve(parentValue, {id}) {
+                return axios.delete(`http://localhost:3000/users/${id}`)
+                    .then(res => res.data);
+            }
         }
     }
 });
